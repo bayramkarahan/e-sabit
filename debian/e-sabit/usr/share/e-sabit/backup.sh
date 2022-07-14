b666e89b04241b214347a4277e105be78703dbeb
karahan <bayramk@gmail.com>
2022-07-14 15:39:29 +0300

    2.1

--- debian/e-sabit/usr/share/e-sabit/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-usr=$(cat /usr/share/e-sabit/e-sabit.conf|grep "kullaniciDizin"|cut -d'|' -f 2)
-
-user=$(echo "$usr"|cut -d'/' -f 3)
-bilgi=$(cat /usr/share/e-sabit/e-sabit.conf|grep "yedekState|1")
-
-if [ -n "$user" ]; then
-	rsync -apoglr --delete /home/"$user"/ /var/backups/e-sabit/"$user"/
-fi
