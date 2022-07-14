e966c2544acbfea1bff0338fa570390cad532c47
karahan <bayramk@gmail.com>
2022-07-14 14:42:52 +0300

    2.1

--- restorebutton.sh
+++ restorebutton.sh
@@ -6,7 +6,7 @@ bilgi=$(cat /usr/share/e-sabit/e-sabit.conf|grep "yedekState|1")
 
 if [ "yedekState|1" == "$bilgi" ]; then
 
-	if [ -f /var/backups/"$user" ]; then
+	if [ -d /var/backups/e-sabit/"$user" ]; then
 		rsync -apoglr --delete /var/backups/e-sabit/"$user"/ /home/"$user"/
 		chown -R "$user":"$user" /home/"$user"/
 		chmod -R 744 /home/"$user"/
